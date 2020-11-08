9e646a5a567ae6cbcf60810060a5d68d0bb71fff
Anky209e <71585377+Anky209e@users.noreply.github.com>
2020-11-08 14:58:33 +0530

    final cal with goto

--- calcbase.c
+++ calcbase.c
@@ -3,13 +3,12 @@ int sum()
 {
     int num1;
     int num2;
-    
-    
+
     printf("Enter the 1st number:\n");
     scanf("%d", &num1);
     printf("Enter the 2nd number:\n");
     scanf("%d", &num2);
-    printf("Sum is %d\n",num1+num2);
+    printf("Sum is %d\n\n\n", num1 + num2);
     return 0;
 }
 int sub()
@@ -19,8 +18,8 @@ int sub()
     printf("Enter the 1st number:\n");
     scanf("%d", &s1);
     printf("Enter the 2nd number:\n");
-    scanf("%d",&s2);
-    printf("answer is %d\n",s1-s2);
+    scanf("%d", &s2);
+    printf("answer is %d\n\n\n", s1 - s2);
     return 0;
 }
 int multi()
@@ -31,7 +30,7 @@ int multi()
     scanf("%f", &m1);
     printf("Enter the 2nd number:\n");
     scanf("%f", &m2);
-    printf("Multiplication of %f and %f is %f\n", m1, m2, m1 * m2);
+    printf("Multiplication of %f and %f is %f\n\n\n", m1, m2, m1 * m2);
     return 0;
 }
 int div()
@@ -42,7 +41,7 @@ int div()
     scanf("%f", &d1);
     printf("Enter the number:\n");
     scanf("%f", &d2);
-    printf("When %f is divided by %f it gives %f\n", d1, d2, d1 / d2);
+    printf("When %f is divided by %f it gives %f\n\n\n", d1, d2, d1 / d2);
     return 0;
 }
 int area()
@@ -50,7 +49,7 @@ int area()
     float side;
     printf("Enter the length of side:\n");
     scanf("%f", &side);
-    printf("Area of square is %f:\n", side * side);
+    printf("Area of square is %f:\n\n\n", side * side);
     return 0;
 }
 int average()
@@ -68,45 +67,51 @@ int average()
     scanf("%f", &n3);
     n = n1 + n2 + n3;
     avg = n / 3;
-    printf("Average of these no is %f\n", n);
+    printf("Average of these no is %f\n\n\n", n);
     return 0;
 }
 int main()
 {
-int ch;
-
-
-
-printf("Enter one of following:\n1) sum of 2 no.\n2)subtracting 2 no\n3) multiply 2 no\n4) divide \n5) area of square\n6)average of 3 no\n");
-scanf("%d", &ch);
-if (ch == 1)
-{
-    sum();
-}
-else if (ch == 2)
-{
-    sub();
-}
-else if (ch == 3)
-{
-    multi();
-}
-else if (ch == 4)
-{
-    div();
-}
-else if (ch == 5)
-{
-    area();
     
-}
-else if (ch == 6)
-{
-    average();
-}
-else
-{
-    printf("Invalid Input try again\n");
-}
-return 0;
+    int ch;
+    label1:
+
+    printf("Enter one of following:\n1) sum of 2 no.\n2)subtracting 2 no\n3) multiply 2 no\n4) divide \n5) area of square\n6)average of 3 no\n");
+    scanf("%d", &ch);
+    if (ch == 1)
+    {
+        sum();
+        goto label1;
+    }
+    else if (ch == 2)
+    {
+        sub();
+        goto label1;
+    }
+    else if (ch == 3)
+    {
+        multi();
+        goto label1;
+    }
+    else if (ch == 4)
+    {
+        div();
+        goto label1;
+    }
+    else if (ch == 5)
+    {
+        area();
+        goto label1;
+    }
+    else if (ch == 6)
+    {
+        average();
+        goto label1;
+    }
+    else
+    {
+        printf("Invalid Input try again\n");
+        goto label1;
+    }
+    return 0;
 }
